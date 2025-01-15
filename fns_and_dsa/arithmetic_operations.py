012f26398a42d3206bd98bcd2c2730da168f6f13
Rose Njambi <njambi.r@gmail.com>
2025-01-15 15:02:06 +0300

    edit3

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
     if operation=="add":
         result=num1 + num2
     elif operation=="subtract":
-        result=num2 - num1
+        result=num1 - num2
     elif operation=="multiply":
         result=num1 * num2
     elif operation=="divide":
@@ -10,5 +10,7 @@ def perform_operation(num1, num2, operation):
            result=print ("Cannot divide bu zero")
         else:
            result=num1 / num2
+    else:
+        result=print("Enter valid arguments")
     return result
     
