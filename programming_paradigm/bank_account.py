561ddb82e0845d80ad654d09ccc2dc08cb4c3a8d
Rose Njambi <njambi.r@gmail.com>
2025-01-23 17:16:22 +0300

    edit2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,13 +6,15 @@ class BankAccount:
 #2: Encapsulation and Behaviors
     def deposit (self, amount):
         self.account_balance+=amount
-        print(f"Deposited: ${amount}")
+        #print(f"Deposited: ${amount}")
     def withdraw(self, amount):
         if self.account_balance>=amount:
             self.account_balance-=amount
-            print(f"Withdrew: ${amount}")
+            #print(f"Withdrew: ${amount}")
+            return True
         else:
-            print("Insufficient funds.")
+            #print("Insufficient funds.")
+            return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
             
