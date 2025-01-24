30d6fb9e1aedeeec9fd4731715dc73c15140ceef
Rose Njambi <njambi.r@gmail.com>
2025-01-24 14:28:06 +0300

    new file

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,15 @@
+# Defining a funcion that performs division 
+# and handles potential errors.
+def safe_divide(numerator, denominator): 
+    try:
+        numerator=float(numerator)
+        denominator=float(denominator)
+        result=numerator / denominator
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+    else:
+        print(f"The result of the division is {result:.1f}")
+    
+safe_divide (10,3)  
