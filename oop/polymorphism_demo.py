71ebc9e19ed2b69e025bb17ff948f0d1e87fac2a
Rose Njambi <njambi.r@gmail.com>
2025-01-26 17:45:41 +0300

    edit1

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -18,5 +18,5 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return math.pi*self.radius*self.radius
+        return math.pi*self.radius**2
 
