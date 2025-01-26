78e365e96fa6e6d19ef889e8ef549cfa881668b3
Rose Njambi <njambi.r@gmail.com>
2025-01-26 17:43:56 +0300

    new file

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+#Import library
+import math
+
+#Base class
+class Shape:
+    def area (self):
+        raise NotImplementedError
+
+#Derived class
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi*self.radius*self.radius
+
